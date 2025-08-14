a7f91c98f7545dd2bb79d27c7acccfbae79ae0ad
taehee <taeheecafe@gmail.com>
2025-08-14 17:40:03 +0900

    자바 첫 수업

---
+++ consoleTest/src/consoleTest/ConsoleTask.java
@@ -0,0 +1,9 @@
+package consoleTest;
+
+//저의 이름은 OOO이고 , 
+// 나이는 OO살 입니다.
+public class ConsoleTask {
+	public static void main(String[] args) {
+		System.out.print("저의 이름은 이태희이고, \n나이는 27살 입니다.");
+	}
+}
