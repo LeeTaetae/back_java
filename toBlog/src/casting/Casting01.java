3f1753d800fda7aaa30cc66b8b8e70074ca3ec5d
taehee <taeheecafe@gmail.com>
2025-10-07 23:58:03 +0900

    형변환 블로그

---
+++ toBlog/src/casting/Casting01.java
@@ -0,0 +1,13 @@
+package casting;
+
+public class Casting01 {
+	public static void main(String[] args) {
+		
+		int num = 10;
+		
+		double result = num;
+		
+		System.out.println("num의 값 : " + num);
+		System.out.println("result의 값 : " + result);
+	}
+}
---
+++ toBlog/src/casting/Casting02.java
@@ -0,0 +1,12 @@
+package casting;
+
+public class Casting02 {
+	public static void main(String[] args) {
+		
+		double d1 = 10.0;
+		
+		int num = (int)d1;
+		
+		System.out.println(num);
+	}
+}
